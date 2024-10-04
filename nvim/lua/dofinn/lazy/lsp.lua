5c151a8b72185bfce292d8947d211829b8e0ebcc
finnd3 <167492412+finnd3@users.noreply.github.com>
2024-10-04 16:19:56 +1000

    latest work
    Signed-off-by: finnd3 <167492412+finnd3@users.noreply.github.com>

--- nvim/lua/dofinn/lazy/lsp.lua
+++ nvim/lua/dofinn/lazy/lsp.lua
@@ -81,7 +81,7 @@ return {
       require('mason-lspconfig').setup({
         -- Replace the language servers listed here
         -- with the ones you want to install
-        ensure_installed = {'lua_ls', 'rust_analyzer', 'gopls'},
+        ensure_installed = {'lua_ls', 'rust_analyzer', 'gopls', 'marksman'},
         handlers = {
           function(server_name)
             require('lspconfig')[server_name].setup({})
---
+++ nvim/todo.md
@@ -0,0 +1,6 @@
+## Todo
+
+- ftplugin settings for common file types
+- go IDE - imports, fmt, debugger/delv
+- LSP autocomplete pathoegen guy
+- go to definition functionality
