c4a79419b3c9b4a6e39d39262d216ff102f331f0
dofinn <dom.finn00@gmail.com>
2024-10-13 23:22:59 +1100

    lsp quality of life

--- nvim/lua/dofinn/remap.lua
+++ nvim/lua/dofinn/remap.lua
@@ -1,3 +1,8 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pe", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>la", vim.cmd.Lazy)
+vim.keymap.set("n", "<CR><leader>", vim.cmd.Auto)
+
+-- swap current line with below line
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+-- vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
--- nvim/todo.md
+++
@@ -1,6 +0,0 @@
-## Todo
-
-- ftplugin settings for common file types
-- go IDE - imports, fmt, debugger/delv
-- LSP autocomplete pathoegen guy
-- go to definition functionality
