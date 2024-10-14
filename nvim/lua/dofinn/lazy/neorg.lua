a88f262771337d5c65321930ca46a91d051b8d7e
finnd3 <167492412+finnd3@users.noreply.github.com>
2024-10-14 22:08:59 +1100

    QoL and neorg
    Signed-off-by: finnd3 <167492412+finnd3@users.noreply.github.com>

--- nvim/lua/dofinn/lazy/neorg.lua
+++ nvim/lua/dofinn/lazy/neorg.lua
@@ -2,5 +2,31 @@ return {
   "nvim-neorg/neorg",
   lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
   version = "*", -- Pin Neorg to the latest stable release
-  config = true,
+  dependencies = { { "nvim-lua/plenary.nvim" }, { "nvim-neorg/neorg-telescope" } },
+  config = function ()
+    require("neorg").setup({
+      load = {
+        ["core.defaults"] = {},
+        ["core.export"] = {},
+        ["core.concealer"] = {
+          config = {
+            folds = false,
+          },
+        },
+        ["core.dirman"] = {
+          config = {
+            workspaces = {
+              notes = "~/notes",
+            },
+          },
+        },
+        ["core.integrations.telescope"] = {},
+      },
+    })
+    vim.keymap.set("n", "<localleader>lf", "<Plug>(neorg.telescope.find_linkable)")
+    vim.keymap.set("n", "<localleader>li", "<Plug>(neorg.telescope.insert_link)")
+    vim.keymap.set("n", "<localleader>lf", "<Plug>(neorg.telescope.insert_file_link)")
+    vim.keymap.set("n", "<localleader>ws", "<Plug>(neorg.telescope.switch_workspace)")
+    vim.keymap.set("n", "<localleader>sh", "<Plug>(neorg.telescope.search_headings)")
+  end
 }
--- nvim/lua/dofinn/remap.lua
+++ nvim/lua/dofinn/remap.lua
@@ -1,4 +1,5 @@
 vim.g.mapleader = " "
+vim.g.maplocalleader = ","
 vim.keymap.set("n", "<leader>pe", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>la", vim.cmd.Lazy)
 vim.keymap.set("n", "<CR><leader>", vim.cmd.Auto)
