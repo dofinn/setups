45f19003361938086ff751ab8adec2b3242ef2d9
dofinn <dom.finn00@gmail.com>
2025-03-07 16:02:45 +1000

    suppres netrw

--- nvim/lua/dofinn/init.lua
+++ nvim/lua/dofinn/init.lua
@@ -52,9 +52,9 @@ autocmd('LspAttach', {
   end
 })
 
--- vim.g.netrw_browse_split = 0
--- vim.g.netrw_banner = 0
--- vim.g.netrw_winsize = 25
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25
 
 autocmd("BufWritePre", {
   pattern = "*.go",
